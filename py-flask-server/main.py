48f3e43abab62a4594543a395697a9ba2c186e92
bh0 <bryce13h0@gmail.com>
2020-05-18 22:07:31 +0100

    removed replaceForUrl function

--- py-flask-server/main.py
+++ py-flask-server/main.py
@@ -2,13 +2,6 @@ import flask
 from flask import request
 import math 
 
-def replaceForUrl(before, after, string):
-    if before in string:
-        return string.replace(before, after)
-        print(f"s {string}")
-    else:
-      return string
-
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 @app.route('/', methods=['GET'])
@@ -16,8 +9,7 @@ def home():
     return f"endpoint: http://whitesmokeremorsefuldeal--five-nine.repl.co/calc/98*9div9+math.ceil(0.2)" 
 
 replacements = {
-  "div": "/",
-  "dot": "."
+  "div": "/", "dot": "."
 } 
 
 @app.route('/calc/<calc_input>', methods=['GET'])
@@ -26,14 +18,11 @@ def calc(calc_input):
     str_code = str(code)
     # to handle division, I will simply replace the word div with "/" - ideally the user should be able to simply type "/" - maybe this would be done on the AHK (client end) 
     final_code = str_code
-    # for key, value in replacements.items():
-    # print (key, value)
-    # final_code = replaceForUrl("div", "/", str_code) 
-    dot_final_code = replaceForUrl("dot", ".", str_code) 
-    div_final_code = replaceForUrl("div", "/", dot_final_code)
-    # output = eval(str(2/2)) # eval(final_code)      
-    final_code = div_final_code 
-    output = eval(final_code) # eval(final_code)      
+    for key, value in replacements.items():
+      if key in final_code:
+        final_code = final_code.replace(key, value)
+
+    output = eval(final_code) 
     return f"output {output}"
     
 app.run(host='0.0.0.0', port=8080)
